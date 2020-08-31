33fbf0d3970a2825f2bdc0edd182e7cc2a929369
Caio Guedes <caio.cesar.g.souza@gmail.com>
2020-08-31 17:11:54 -0300

    hue

---
+++ src/c/core.clj
@@ -0,0 +1,42 @@
+(ns c.core
+  (:require [clj-yaml.core :as y]
+            [clojure.term.colors :as c]
+            [clojure.string :as s])
+  (:gen-class))
+
+(defn replace-variables [value]
+  (if-let [match (re-find #"\$\{([^}^{]+)\}" value)]
+    (let [pair (s/split (last match) #":")]
+      (recur (s/replace value (first match) (last pair))))
+    value))
+
+(defn load-bootstrap []
+  (y/parse-string (slurp "src/main/resources/bootstrap.yml")))
+  
+(defn get-config [bootstrap]
+  (get-in bootstrap [:spring :cloud :config]))
+
+(defn get-vault [bootstrap]
+  (get-in bootstrap [:spring :cloud :vault]))
+
+(defn create-vault-url [vault]
+  (format "%s://%s:%s/ui/vault/secrets/secret/show/%s"
+          (:scheme vault)
+          (:host vault)
+          (:port vault)
+          (get-in vault [:generic :application-name])))
+
+(defn create-config-url [config]
+  (format "%s/%s/%s" (:uri config) (:name config) "default"))
+
+(defn -main [& args]
+  (let [bootstrap (load-bootstrap)
+        config (get-config bootstrap)
+        vault (get-vault bootstrap)]
+    (println (c/yellow "Spring Cloud Config:"))
+    (println (c/yellow "Url:") (-> config create-config-url replace-variables))
+    ;; (replace-variables (create-config-url config))
+    (println "")
+    (println (c/yellow "Spring Cloud Vault:"))
+    (println (c/yellow "Token:") (replace-variables (:token vault)))
+    (println (c/yellow "Url:") (-> vault create-vault-url replace-variables))))
