3c8acf723d974e402fa7cb41d6d13a0f1aee23bd
Caio Guedes <caio.cesar.g.souza@gmail.com>
2020-10-04 17:26:47 -0300

    refactor println to prn and update project.clj file

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject c "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject c "0.0.1-SNAPSHOT"
+  :description "Small cli tool for personal ordinary tasks"
+  :url "http://github.com/caioguedes/c"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [clj-commons/clj-yaml "0.7.0"]
                  [clojure-term-colors "0.1.0"]]
   :main ^:skip-aot c.core
